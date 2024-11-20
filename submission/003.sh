64cb8bad5506d06dc6cd2729286acb647fef449e
Rafael Turon Balbino <rafael.turon@gmail.com>
2024-11-20 08:59:18 -0300

    q#003

--- submission/003.sh
+++ submission/003.sh
@@ -1,2 +1,9 @@
 # How many new outputs were created by block 123,456?
-bitcoin-cli getblockhash 123456 | xargs bitcoin-cli getblock | jq '.tx | length'
+# Step 1: Get the block hash for block 123,456
+BLOCK_HASH=$(bitcoin-cli getblockhash 123456)
+# Step 2: Get the block details (including transactions)
+BLOCK_DETAILS=$(bitcoin-cli getblock $BLOCK_HASH 2)
+# Step 3: Count the total number of outputs (vout entries) across all transactions
+TOTAL_OUTPUTS=$(echo "$BLOCK_DETAILS" | jq '[.tx[].vout[]] | length')
+# Response
+echo $TOTAL_OUTPUTS
